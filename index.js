a2ae932c02911274fb01e1aa30b4edda5d549b28
Daloar Hossan <daloarhossan128@gmail.com>
2022-05-01 00:42:18 +0600

    db-add

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log('connect');
 
-
 const run=async()=>{
 	try{
  client.connect();
