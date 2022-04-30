cd961661c37854fb497e367aa028236dce2c3032
Daloar Hossan <daloarhossan128@gmail.com>
2022-05-01 00:31:52 +0600

    db-connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port=process.env.PORT || 5000;
 const app=express();
@@ -10,6 +11,23 @@ app.get('/',(req,res)=>{
 	res.send('Running');
 })
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhjsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('connect');
+
+
+const run=async()=>{
+	try{
+ client.connect();
+ 
+	}
+	catch{
+
+	}
+}
+run()
 
 
 app.listen(port,()=>{
