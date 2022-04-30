92b2e4e6443ff89d4bc940143a9768e044bbaad4
Daloar Hossan <daloarhossan128@gmail.com>
2022-05-01 00:39:08 +0600

    db-add

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.get('/',(req,res)=>{
 	res.send('Running');
 })
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qhjsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log('connect');
